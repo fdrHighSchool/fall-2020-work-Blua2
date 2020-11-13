f0d5299a15b74704b41074ecee4e0a337c798e3b
Blua2 <72803885+Blua2@users.noreply.github.com>
2020-11-13 18:54:44 -0500

    Add files via upload

--- FracCalc/FC.java
+++ FracCalc/FC.java
@@ -1,30 +1,62 @@
 import java.util.*;
 
 public class FC {
+    static ArrayList<String> OPR = new ArrayList<String>();//Global ArrayList was used so I can call it within the produceAnswer method and display it in my main method.
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
+        Scanner S = new Scanner(System.in);    
+        Boolean start = true;
 
-        System.out.println("\nPlease input your expression : ");
-        String exp = S.nextLine();
-        
-        /*
-        Match any characters that are provided within the given String, if the string does not contain any of the valid characters continue to else.
-        Using .matches can cut down on any unnecessary methods that would check for valid arithmetic.
-        */
-        if (exp.matches(".*[+-/*].*")){  
-            produceAnswer(exp);
-            System.out.println("\nRESULT : "+ produceAnswer(exp));
-        }
-        else {
-            System.out.println("\nIllegal arguement : " + "String "+ exp + " does not contain valid arithmetic (+ - / *)"); 
-        }
+        while(start){
+            System.out.print("\nInput: ");
+            String exp = S.nextLine();
+           // exp = exp.replaceAll("\\s","");//This calculator will not be using blank spaces to indicate to split since it can cause problems when reading characters.
+
+            if (exp.indexOf("+") >= 0 || exp.indexOf("-") >= 0 || exp.indexOf("*") >= 0 || exp.indexOf("/") >= 0){//indexOf will see if the inputed String contains any of the strings to compare.
+                produceAnswer(exp);
+                if(OPR.get(2).indexOf("_") >= 0){
+                    System.out.println("\nWHOLE: " + OPR.get(3) +"\nNUMERATOR: " + OPR.get(4) + "\nDENOMINATOR: " + OPR.get(5));//Print results
+                }
+                else if(OPR.get(2).indexOf("/") >= 1){
+                    System.out.println("\nWHOLE NUMBER: 0" + "\nNUMERATOR: " + OPR.get(3) + "\nDENOMINATOR: " + OPR.get(4));//Print results
+                }
+                else {
+                    System.out.println("\nWHOLE NUMBER: " + OPR.get(2) +"\nNUMERATOR: 0"  + "\nDENOMINATOR: 1");//Print results
+                }
+            }
+            else if(exp.equalsIgnoreCase("quit")){
+                System.out.println("Goodbye");
+                start = false;
+            }
+            else if(exp.equalsIgnoreCase("off")){
+                System.out.println("OFF");
+                exp = S.nextLine();
+            }
+            else if(exp.equalsIgnoreCase("on")){
+                System.out.println("ON");
+                exp = S.nextLine();
+            }
+            else {
+                System.out.println("\nIllegal arguement : " + "String "+ exp + " does not contain valid arithmetic (+ - / *)"); 
+            }
+            OPR.clear();
+    }
         S.close();
     }//end of main
 
-    public static String produceAnswer(String exp){
-        String[] value = exp.split("\\s+");// Split String by the occurrences of blank spaces
-        String opr2 = value[2];// Store the third value of the seperated String in opr2
+    public static ArrayList<String> produceAnswer(String exp){
+        OPR.addAll(Arrays.asList(exp.split("\\s+")));
+        Collections.swap(OPR, 0, 1); // Swapping operand 1 with operator to keep operator the same in all scenarios.
 
-        return opr2;
-    }
+        if(OPR.get(2).indexOf("_") >= 0){//Check if operators are fractions
+            OPR.addAll(Arrays.asList(OPR.get(2).split("[_/]")));
+            return OPR;
+        }
+        else if(OPR.get(2).indexOf("/") >= 0 ){
+            OPR.addAll(Arrays.asList(OPR.get(2).split("[/]")));
+            return OPR;
+        }
+        else{
+            return OPR;
+        }
+    }    
 }//end of class
