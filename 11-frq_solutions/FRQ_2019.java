c434a653fe87d4f26a7cb7e3182d51a3d342f007
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-01-22 00:04:35 -0500

    Update FRQ_2019.java

--- 11-frq_solutions/FRQ_2019.java
+++ 11-frq_solutions/FRQ_2019.java
@@ -1,24 +1,24 @@
 public class FRQ_2019 {
     public static void main(String[] args){
         System.out.println(numberOfLeapYears(2002, 2008));
-    }
+    }//end main method
 
     public static int numberOfLeapYears(int year1, int year2){
         int dist = 0; //distance
 
-        for(int i = year1; i < year2; i++){
+        for(int i = year1; i < year2; i++){//Inputs 2 years as a range and increments distance if the current iteration is a leap year
             if(isLeapYear(i)){
                 dist++;
             }
-        }
+        }//End for loop
         return dist;
-    }
+    }//End of method numberOfLeapYears
 
-    public static int dayOfWeek(int month, int day, int year){
+    public static int dayOfWeek(int month, int day, int year){//Intakes 3 parameters and inputs them into provided methods
         int date = 0;
         int firstDAY = firstDayOfTheYear(year);
         int dayYear = dayOfYear(month, day, year);
         date = (firstDAY + dayYear - 1) % 7;
         return date;
-    }
-}
+    }//End of method dayOfWeek
+}//End class
